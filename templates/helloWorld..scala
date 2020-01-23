2d9ac8d0e8488dbfe988cfae1957211aa5198ebb
Marcin Polak <mapoart@gmail.com>
2020-01-23 18:44:41 +0100

    fixed scala does not take arguments

--- templates/helloWorld..scala
+++ templates/helloWorld..scala
@@ -1,6 +1,13 @@
 import scala.io.StdIn
 import ujson._
 
+// Below needs to be to avoid argument issues with eg --test
+// https://github.com/nexssp/language_scala/wiki/Script-scala-does-not-take-arguments-error---ammonite
+@main
+def main(s: String*): Unit = {
+//   println(s)
+}
+
 val NexssStdin = StdIn.readLine()
 
 val NexssStdout = ujson.read(NexssStdin)
