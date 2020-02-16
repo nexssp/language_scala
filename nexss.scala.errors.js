2b9962c64b8d59c3828d48cfd4edb89f26fb99ad
Marcin Polak <mapoart@gmail.com>
2020-02-16 15:44:55 +0100

    Added info about very first start of the scala program

--- nexss.scala.errors.js
+++ nexss.scala.errors.js
@@ -1,3 +1,5 @@
 module.exports = {
-  "error here '(.*?)'": "nexss install java <module> OR sbt install <module>"
+  "error here '(.*?)'": "nexss install java <module> OR sbt install <module>",
+  "interpBridge.sc":
+    "For the very first time scala shows some installation/compiling messages. Please re-run the program."
 };
