f0992c0dfb3e70bf72fcdff2f14367d9cd9f30b5
Marcin Polak <mapoart@gmail.com>
2020-10-11 19:22:15 +0200

    removed unnecessary sudo and run command.

--- scala.darwin.nexss.config.js
+++ scala.darwin.nexss.config.js
@@ -1,43 +1,10 @@
 let languageConfig = Object.assign({}, require("./scala.win32.nexss.config"));
 languageConfig.compilers = {
   brew: {
-    install: "brew update && brew install scala",
+    install: "brew install ammonite-repl",
     command: "amm",
     args: "--no-remote-logging <file>", //args: "-s <file>",
     help: ``,
-  },
-  amm: {
-    install: "scoop install scala sbt ammonite",
-    command: "amm",
-    args: "--no-remote-logging <file>", //args: "-s <file>",
-    help: ``,
-  },
-};
-languageConfig.languagePackageManagers = {
-  sbt: {
-    installation: "scoop install sbt",
-    messageAfterInstallation: "",
-    installed: "sbt installed",
-    search: "sbt search",
-    install: "sbt install",
-    uninstall: "sbt remove",
-    help: "sbt",
-    version: "sbt version",
-    init: () => {
-      if (
-        !require("fs").existsSync(
-          require("path").join(process.cwd(), "build.sbt")
-        )
-      ) {
-        require("child_process").execSync("sbt new myapp -c", {
-          stdio: "inherit",
-        });
-        console.log("initialized sbt project.");
-      } else {
-        console.log("sbt already initialized.");
-      }
-    },
-    else: "sbt",
   },
 };
 
--- scala.linux.nexss.config.js
+++ scala.linux.nexss.config.js
@@ -1,14 +1,14 @@
 let languageConfig = Object.assign({}, require("./scala.win32.nexss.config"));
-
-const os = require("@nexssp/os");
+const os = require(`${process.env.NEXSS_SRC_PATH}/node_modules/@nexssp/os/`);
 const sudo = os.sudo();
+// const os = require("@nexssp/os");
 
 const distName = os.name();
 languageConfig.dist = distName;
 
 languageConfig.compilers = {
   scala: {
-    install: "apt-get install -y scala",
+    install: `${sudo}sh -c '(echo "#!/usr/bin/env sh" && curl -L https://github.com/lihaoyi/Ammonite/releases/download/2.2.0/2.13-2.2.0) > /usr/local/bin/amm && chmod +x /usr/local/bin/amm'`,
     command: "amm",
     args: "--no-remote-logging <file>", //args: "-s <file>",
     help: ``,
@@ -17,7 +17,7 @@ languageConfig.compilers = {
 
 switch (distName) {
   case os.distros.ARCH:
-    languageConfig.compilers.scala.install = "pacman -S --noconfirm ammonite"; // error: package org.json does not exist
+    languageConfig.compilers.scala.install = `${sudo}pacman -S --noconfirm ammonite`; // error: package org.json does not exist
     break;
   default:
     languageConfig.compilers.scala.install = os.replacePMByDistro(
